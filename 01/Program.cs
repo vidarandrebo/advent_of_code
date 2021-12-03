423203326ee217a949b00cfaeed1f7465fc477c7
vidarandrebo <vidarandre.bo@protonmail.com>
2021-12-03 13:35:53 +0100

    init

---
+++ 01/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+var lines = await File.ReadAllLinesAsync("val.txt");
+var increases = 0;
+for (int i = 3; i < lines.Length; i++) {
+    if(int.Parse(lines[i]) > int.Parse(lines[i-3])){
+        increases++;
+    }
+}
+Console.WriteLine(increases);
+*/
+
+var lines = await File.ReadAllLinesAsync("val.txt");
+var increases = 0;
+for (int i = 1; i < lines.Length; i++) {
+    if(int.Parse(lines[i]) > int.Parse(lines[i-1])){
+        increases++;
+    }
+}
+Console.WriteLine(increases);
---
+++ 02/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Name;
+await MyClass.Part1();
+Console.WriteLine();
+await MyClass.Part2();
+
+public struct Coordinate {
+    public int x {get;set;}
+    public int y {get;set;}
+    public int aim {get;set;}
+}
+
+namespace Name
+{
+    public class MyClass {
+        public static async Task Part1() {
+            var lines = await File.ReadAllLinesAsync("val.txt");
+            var coordinates = new Coordinate();
+            foreach(var line in lines) {
+                var lineValues = line.Split();
+                switch(lineValues[0]) {
+                    case "forward":
+                        coordinates.x += int.Parse(lineValues[1]);
+                        break;
+                    case "down":
+                        coordinates.y += int.Parse(lineValues[1]);
+                        break;
+                    case "up":
+                        coordinates.y -= int.Parse(lineValues[1]);
+                        break;
+
+                    default:
+                    break;
+                }
+            }
+            Console.WriteLine($"{coordinates.x}\t{coordinates.y}");
+            Console.WriteLine(coordinates.x * coordinates.y);
+        }
+
+        public static async Task Part2() {
+            var lines = await File.ReadAllLinesAsync("val.txt");
+            var coordinates = new Coordinate();
+            foreach(var line in lines) {
+                var lineValues = line.Split();
+                switch(lineValues[0]) {
+                    case "forward":
+                        coordinates.x += int.Parse(lineValues[1]);
+                        coordinates.y += int.Parse(lineValues[1]) * coordinates.aim;
+                        break;
+                    case "down":
+                        coordinates.aim += int.Parse(lineValues[1]);
+                        break;
+                    case "up":
+                        coordinates.aim -= int.Parse(lineValues[1]);
+                        break;
+
+                    default:
+                    break;
+                }
+            }
+            Console.WriteLine($"{coordinates.x}\t{coordinates.y}");
+            Console.WriteLine(coordinates.x * coordinates.y);
+        }
+    }
+    
+}
