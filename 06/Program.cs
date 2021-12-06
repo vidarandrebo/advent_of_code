f1304259cdb800eca37b2f52ebdc09da552b205f
vidarandrebo <va.bo@stud.uis.no>
2021-12-06 10:49:54 +0100

    autoformat and optimize

--- 06/Program.cs
+++ 06/Program.cs
@@ -2,39 +2,46 @@
 Console.WriteLine($"FishCount: {fishCount}");
 namespace Name
 {
-    public static class Fish {
-        public static long Run() {
+    public static class Fish
+    {
+        public static long Run()
+        {
             var file = File.ReadAllLines("val.txt");
             var fishDict = new Dictionary<int, long>();
-            var fish = file[0]
-                .Split(',')
-                .Select(x => byte.Parse(x))
-                .ToList();
-            for (int i = 0; i < 9; i++) {
+            for (int i = 0; i < 9; i++)
+            {
                 fishDict[i] = 0;
             }
-            foreach (var value in fish) {
-                fishDict[value]++;
-            }
-            for (int i = 0; i < 256; i++) {
+            file[0]
+                .Split(',')
+                .Select(x => byte.Parse(x))
+                .ToList()
+                .ForEach(x => fishDict[x]++);
+
+            for (int i = 0; i < 256; i++)
+            {
                 fishDict = NextDay(fishDict);
             }
+
             long totalCount = 0;
-            for (int i = 0; i < 9; i++) {
+            for (int i = 0; i < 9; i++)
+            {
                 totalCount += fishDict[i];
             }
+
             return totalCount;
+        }
+        public static Dictionary<int, long> NextDay(Dictionary<int, long> fish)
+        {
+            var newDict = new Dictionary<int, long>();
+            for (int i = 8; i > 0; i--)
+            {
+                newDict[i - 1] = fish[i];
             }
-            public static Dictionary<int, long> NextDay(Dictionary<int, long> fish) {
-                long newFish = 0;
-                var newDict = new Dictionary<int, long>();
-                for (int i = 8; i > 0; i--) {
-                    newDict[i-1] = fish[i];
-                }
-                newFish = fish[0];
-                newDict[6] += fish[0];
-                newDict[8] = newFish;
-                return newDict;
-            }
-    } 
+            var newFish = fish[0];
+            newDict[6] += fish[0];
+            newDict[8] = newFish;
+            return newDict;
+        }
+    }
 }
