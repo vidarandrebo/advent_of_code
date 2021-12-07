ad1255aa08753876ac85e584331f596a205f32ca
vidarandrebo <va.bo@stud.uis.no>
2021-12-07 10:13:26 +0100

    finished with 07

---
+++ 07/Program.cs
@@ -0,0 +1,13 @@
+﻿var file = File.ReadAllLines("val.txt");
+var numbers = file[0]
+    .Split(',')
+    .Select(x => int.Parse(x))
+    .ToArray();
+var results = new List<int>();
+for (int i = 0; i < numbers.Max(); i++) {
+    results.Add(numbers
+        .Select(x => (Math.Abs(x-i)*((Math.Abs(x-i)+1))/2))
+        .Sum()
+    );
+}
+Console.WriteLine(results.Min());
